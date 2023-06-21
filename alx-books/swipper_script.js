3431c14a54dc5193367ba1eb0218e74e066a1941
José Alexandre <jose.antunes@qitech.com.br>
2023-06-21 02:54:10 -0300

    adding carrossel sections into the project

--- alx-books/swipper_script.js
+++ alx-books/swipper_script.js
@@ -24,3 +24,30 @@ const swiper = new Swiper('.swiper', {
         type: 'bullets',
       }
   })
+
+  const swiper_two = new Swiper('.swiper', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      }
+  })
